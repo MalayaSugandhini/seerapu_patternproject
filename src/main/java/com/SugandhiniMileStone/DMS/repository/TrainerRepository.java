ee95d99ca32b9d66035aa408e219cdb15fab95dd
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-04-17 17:13:45 -0500

    adding project

---
+++ src/main/java/com/SugandhiniMileStone/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.SugandhiniMileStone.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.SugandhiniMileStone.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
